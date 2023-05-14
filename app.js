3b1b69d3659d2e37afd59b702753fb3a53a2e824
junfengTan <junfeng.tan@izhijue.com>
2023-05-14 16:16:13 +0800

    tjf

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const Koa = require('koa');
 var Router = require('koa-router');
+const db = require('./database')
 
 const app = new Koa();
 var router = new Router();
 
 router.prefix('/api')
 
-
 router.get('/get', async (ctx) => {
-  ctx.body = "汤睿，我喜欢你";
+  ctx.body = "返回响应数据";
+})
+
+router.get('/user/getInfo', async (ctx) => {
+  const WeChatName = ctx.query.name
+  const res = await db.query(`SELECT * FROM user WHERE WeChatName = ${WeChatName};`)
+  ctx.body = res
 })
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log(`server start at http://127.0.0.1:3000`);
+});
---
+++ database.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+  host: '139.224.61.106',
+  user: 'root',
+  password: 'mysqlU+',
+  database: 'miniProgram'
+})
+
+exports.query = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err)
+        console.log(err, "数据库连接失败");
+        resolve({
+          code: 500
+        })
+      } else {
+        connection.query(sql, values, (err, results) => {
+          if (err) {
+            reject(err)
+            resolve({
+              code: 400
+            })
+          } else {
+            resolve({
+              code: 200,
+              results
+            })
+            connection.release()
+            resolve(rows)
+          }
+          connection.release() // 释放连接池
+        })
+      }
+    })
+  })
+}
