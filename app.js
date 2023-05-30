42437e0c11d4c2646ecc8ea1694536479aa25532
junfengTan <junfeng.tan@izhijue.com>
2023-05-30 14:08:20 +0800

    tjf

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const fs = require('fs')
 const Koa = require('koa');
 var Router = require('koa-router');
 const db = require('./database')
 
+
+const https = require('https')
+const sslify = require('koa-sslify').default
+
+const options = {
+  key: fs.readFileSync('../../ssl/www.xtr327.com.key'),
+  cert: fs.readFileSync('../../ssl/www.xtr327.com.pem')
+}
+
 const app = new Koa();
+
+app.use(sslify())
+
+
+https.createServer(options, app.callback()).listen(3000, (err) => {
+  if (err) {
+    console.log('服务启动出错', err);
+  } else {
+    console.log('运行在' + 3000 + '端口');
+  }	
+})
+
+
 var router = new Router();
 
 router.prefix('/api')
@@ -17,8 +40,17 @@ router.get('/user/getInfo', async (ctx) => {
   ctx.body = res
 })
 
+router.post('/user/setInfo', async (ctx) => {
+  const res = await db.query(`INSET INTO user (WeChatName, openID) values(${ctx.name.WeChatName}, ${ctx.name.openid});`)
+  ctx.body = {
+    status: 200,
+    messgage: 'success',
+    result: res
+  }
+})
+
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, ()=>{
-  console.log(`server start at http://127.0.0.1:3000`);
-});
+// app.listen(3000, ()=>{
+//   console.log(`server start at http://127.0.0.1:3000`);
+// });
