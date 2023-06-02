83e2f8dfe90a84036c5c7293a8a149aa9dc407f2
junfengTan <junfeng.tan@izhijue.com>
2023-06-02 23:58:05 +0800

    tjf

---
+++ config.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const Koa = require('koa')
+var Router = require('koa-router')
+const db = require('./database')
+
+const https = require('https')
+const sslify = require('koa-sslify').default
+
+const options = {
+  key: fs.readFileSync('../../ssl/www.xtr327.com.key'),
+  cert: fs.readFileSync('../../ssl/www.xtr327.com.pem')
+}
+
+const app = new Koa()
+
+app.use(sslify())
+
+https.createServer(options, app.callback()).listen(3000, (err) => {
+  if (err) {
+    console.log('服务启动出错', err)
+  } else {
+    console.log('运行在' + 3000 + '端口')
+  }
+})
+
+var router = new Router()
+
+router.prefix('/api')
+
+app.use(router.routes()).use(router.allowedMethods())
+
+module.exports = {
+  https,
+  router,
+  db,
+  app
+}
---
+++ login.js
@@ -0,0 +1,61 @@
+const { https, router, db, app } = require('./config.js')
+
+module.exports = () => {
+  // 获取openId
+  router.get('/user/getOpenId', async (ctx) => {
+    let code = ctx.query.code
+    let resData
+    await new Promise((resolve, reject) => {
+      const request = https.request(
+        `https://api.weixin.qq.com/sns/jscode2session?appid=wxc0487101e293089a&secret=68083c27a664776591b54d9010fd1499&js_code=${code}&grant_type=authorization_code`,
+        (response) => {
+          response.on('data', (data) => {
+            resData = data.toString()
+            resolve(resData.openid)
+          })
+        }
+      )
+      request.on('error', (error) => {
+        console.log('An error', error)
+        resolve()
+      })
+      request.end()
+    }).then((openid) => {
+      searchUser(openid).then((res) => {
+        if (res.length) {
+          ctx.body = JSON.parse(res[0])
+        } else {
+          ctx.body = '新用户'
+        }
+      })
+    })
+  })
+  // 查找用户信息
+  const searchUser = (openId) => {
+    return new Promise(async (resolve, reject) => {
+      const res = await db.query(
+        `SELECT * FROM user WHERE openID = '${openId}';`
+      )
+      resolve(res)
+    })
+  }
+  // 获取用户信息
+  router.get('/user/getInfo', async (ctx) => {
+    const WeChatName = ctx.query.name
+    const res = await db.query(
+      `SELECT * FROM user WHERE WeChatName = '${WeChatName}';`
+    )
+    ctx.body = res
+  })
+  // 传入用户信息
+  router.post('/user/setInfo', async (ctx) => {
+    const res = await db.query(
+      `INSET INTO user (WeChatName, openID) values(${ctx.name.WeChatName}, ${ctx.name.openid});`
+    )
+    ctx.body = {
+      status: 200,
+      messgage: 'success',
+      result: res
+    }
+  })
+}
