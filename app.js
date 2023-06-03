752515d3d1f723606de9674349a942c60df0f1b4
junfengTan <junfeng.tan@izhijue.com>
2023-06-04 01:42:12 +0800

    tjf

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { https, router, db, app } = require('./config.js')
 const login = require('./login.js')
+const menu = require('./menu.js')
 login()
+menu()
 
 router.get('/get', async (ctx) => {
   ctx.body = '返回响应数据'
---
+++ menu.js
@@ -0,0 +1,21 @@
+const { https, router, db, app } = require('./config.js')
+
+module.exports = () => {
+  // 获取菜单列表
+  router.get('/menu/getMenuList', async (ctx) => {
+    const res = await db.query(`SELECT * FROM menu;`)
+    ctx.body = res.results
+  })
+  // 插入一条菜单
+  router.post('/menu/setMenu', async (ctx) => {
+    const { name, link } = ctx.request.body
+    const res = await db.query(
+      `INSERT INTO menu (name, links) values('${name}', '${link}');`
+    )
+    ctx.body = {
+      status: 200,
+      messgage: 'success',
+      result: res.results[0]
+    }
+  })
+}
