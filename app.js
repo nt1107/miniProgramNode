717993a7effbe02c0d126a8b25b45a4038911fa6
junfengTan <junfeng.tan@izhijue.com>
2023-07-24 18:04:57 +0800

    add file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const { https, router, db, app } = require('./config.js')
 const login = require('./login.js')
 const menu = require('./menu.js')
+const file = require('./file.js')
 login()
 menu()
+file()
 
 router.get('/get', async (ctx) => {
   ctx.body = '返回响应数据'
---
+++ file.js
@@ -0,0 +1,13 @@
+const { https, router, db, app } = require('./config.js')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = () => {
+  // 获取菜单列表
+  router.get('/backgroundVideo', async (ctx) => {
+    const res = await db.query(`SELECT * FROM menu;`)
+    const video = fs.readFileSync(path.join(__dirname, 'background.mp4'))
+    ctx.type = 'video/mp4'
+    ctx.body = video
+  })
+}
