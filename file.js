28ef8a7a6c4d8b0d1d5c6c8cedec7655ff1b9622
junfengTan <junfeng.tan@izhijue.com>
2023-07-24 22:51:31 +0800

    tjf

--- file.js
+++ file.js
@@ -5,10 +5,16 @@ const path = require('path')
 module.exports = () => {
   // 获取菜单列表
   router.get('/backgroundVideo', async (ctx) => {
-    const res = await db.query(`SELECT * FROM menu;`)
-    const video = fs.readFileSync(path.join(__dirname, 'background.mp4'))
+    const stat = fs.statSync(path.join(__dirname, 'background.mp4'))
+    const range = ctx.req.headers.range
+    const parts = range.replace(/bytes=/, '').split('-')
+    const start = Number(parts[0])
+    const end = Number(parts[1]) || stat.size - 1
+    ctx.set('Content-Range', `bytes ${start}-${end}/${stat.size}`)
     ctx.type = 'video/mp4'
     ctx.set('Accept-Ranges', 'bytes')
-    ctx.body = video
+    ctx.status = 206
+    const stream = fs.createReadStream(videoFilePath, { start, end })
+    ctx.body = stream
   })
 }
